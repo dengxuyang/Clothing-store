f6184db59d521052525750a2f98a60b9afad6312
dengxuyang <673303066@qq.com>
2020-10-01 00:56:07 +0800

    init

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function request(config) {
     const instance = axios.create({
-        baseURL: '略略略',
+        baseURL: '就不让你看',
         timeout: 5000
     })
     //请求拦截
